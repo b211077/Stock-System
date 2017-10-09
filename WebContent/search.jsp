ce78ad5687440506e5d29076d878f34e9bded446
kkr <sy1816@hanmail.net>
2017-10-09 21:10:03 +0900

    bs+search

---
+++ WebContent/search.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page import="java.util.*"%>
+<%@ page import="stock.model.*"%>
+<%@ page import="stock.model.dto.*"%>
+<%
+	String json;
+	String data = request.getParameter("id");
+	String type = request.getParameter("type");
+	
+	StockDTO sDAO = new StockDTO();
+	
+	if(data == null){
+		ArrayList<StockDTO> search = null;
+		search = sDAO.searchList("0");
+		json = Converter.convertToJson(search);
+	} else if(data.equals("top10")){
+		ArrayList<StockDTO> search = null;
+		search = sDAO.searchList("1");
+		json = Converter.convertToJson(search);
+	} 	else{
+		StockService search = null;
+		search = sDAO.tourSpotInfo(data);
+		System.out.println(search.getImage_url1());
+		json = Converter.convertToJson(search);
+	}
+%>
+
+<%=json %>
---
+++ src/stock/model/Converter.java
@@ -0,0 +1,38 @@
+package stock.model;
+import java.io.BufferedReader;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Converter {
+	private static Gson gson;
+	static {
+//		gson = new Gson();
+		gson = new GsonBuilder().disableHtmlEscaping().create();
+	}
+	
+	public static String convertToJson(Object obj) {
+		return gson.toJson(obj);
+	}
+	
+	public static <T> T convertFromJson(String json, Class<T> type) {
+		return gson.fromJson(json, type);
+	}
+	
+	public static <T> T convertFromJSONStream(InputStream is, Class<T> type) {
+		try {
+			Reader reader = new BufferedReader(
+					new InputStreamReader(is, "UTF-8"));
+			return gson.fromJson(reader, type);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
